cca9b85e40e7e8b2b14d704bc35b08b7fbd669df
RubenTorres <rtorresgutierrez.guadalupe@alumnado.fundacionloyola.net>
2021-12-21 10:22:03 +0100

    Prueba grid

---
+++ js/js.js
@@ -0,0 +1,12 @@
+/**
+*   @file 
+*   @description
+*   @version 1.0.0
+*   @author Rubén Torres <rtorresgutierrez.guadalupe@alumnado.fundacionloyola.net>
+*   @license GPL-3.0-or-later
+*   Ref: https://spdx.org/licenses/
+*
+*   Ref JSDoc: https://jsdoc.app/
+*/
+'use strict'
+window.onload = iniciar;
